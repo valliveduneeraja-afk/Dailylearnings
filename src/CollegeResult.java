64be87dc6e523e2b4fff87853e0d9a107342fd83
Valli <valliveduneeraja@gmail.com>
2026-01-10 22:58:22 +0530

    CollegeResult

---
+++ src/CollegeResult.java
@@ -0,0 +1,56 @@
+class College {
+    String Name;
+    String Sub;
+    int m1;
+    int m2;
+    String status;
+}
+public class CollegeResult {
+    public static void main(String[] args) {
+        College c1= new College();
+        c1.Name = "Neeru";
+        c1.Sub = "Java";
+        c1.m1 = 86;
+        c1.m2 = 75;
+        c1.status = "Pass";
+        College c2 = new College();
+        c2.Name = "Kyathi";
+        c2.Sub = "Java";
+        c2.m1 = 89;
+        c2.m2 = 80;
+        c2.status = "pass";
+        College c3 = new College();
+        c3.Name = "Priya";
+        c3.Sub = "Java";
+        c3.m1 = 70;
+        c3.m2 = 82;
+        c3.status = "Pass";
+        College c4 = new College();
+        c4.Name = "Salma";
+        c4.Sub = "Java";
+        c4.m1 = 95;
+        c4.m2 = 56;
+        c4.status = "Pass";
+        System.out.println(c1.Name);
+        System.out.println(c1.Sub);
+        System.out.println(c1.m1);
+        System.out.println(c1.m2);
+        System.out.println(c1.status);
+        System.out.println(c2.Name);
+        System.out.println(c2.Sub);
+        System.out.println(c2.m1);
+        System.out.println(c2.m2);
+        System.out.println(c2.status);
+        System.out.println(c3.Name);
+        System.out.println(c3.Sub);
+        System.out.println(c3.m1);
+        System.out.println(c3.m2);
+        System.out.println(c3.status);
+        System.out.println(c4.Name);
+        System.out.println(c4.Sub);
+        System.out.println(c4.m1);
+        System.out.println(c4.m2);
+        System.out.println(c4.status);
+    }
+
+}
