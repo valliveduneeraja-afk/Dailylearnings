9308b95bc7c607f042a302ae2ced902f82af97c2
Valli <valliveduneeraja@gmail.com>
2026-01-10 11:07:47 +0530

    Wearepacticing

---
+++ src/WeAreDoingVariables.java
@@ -0,0 +1,16 @@
+public class WeAreDoingVariables {
+    int y = 2;
+    public static void main(String[] args){
+        ///datatype variablename = value;
+        int a = 12;        ///local vaiable
+        char b = 'A';
+        boolean c = true;
+        float d = 25;
+        double e = 5;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
