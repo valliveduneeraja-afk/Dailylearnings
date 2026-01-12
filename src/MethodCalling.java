44a23bd5133d0a6e14eb9eec642fd98917607175
Valli <valliveduneeraja@gmail.com>
2026-01-12 23:45:22 +0530

    Methods

---
+++ src/MethodCalling.java
@@ -0,0 +1,22 @@
+class Neeraja {
+    public static void main(String[] args) {
+        String msg = "Welcome";
+        System.out.println(msg);
+        Mrgmsg();
+        Noongreet();
+        Eveningwish();
+        Goodnight();
+    }
+    public static void Mrgmsg() {
+        System.out.println("Good Morning");
+    }
+    public static void Noongreet() {
+        System.out.println("Good afternoon");
+    }
+    public static void Eveningwish() {
+        System.out.println("Good evening");
+    }
+    public static void Goodnight() {
+        System.out.println("Good night");
+    }
+}
