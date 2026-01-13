efb6ac61e85c7ae220e0badd8599e24f2f91f959
Valli <valliveduneeraja@gmail.com>
2026-01-13 15:54:42 +0530

    MethodsTask

---
+++ src/MethodsTask.java
@@ -0,0 +1,32 @@
+class Task {
+    public static void main(String[] args) {
+        String Title = "Calculations";
+        System.out.println(Title);
+        Addition();            ///Method calling
+        Substraction();
+        Multiplication();
+        Division();
+        Modulodivision();
+    }
+    public static void Addition() {  /// method defining
+        int a = 3, b = 9;
+        System.out.println(a+b);
+    }
+    public static void Substraction() {
+        int a = 12, b = 10;
+        System.out.println(a-b);
+    }
+    public static void Multiplication() {
+        int a = 45, b = 21;
+        System.out.println(a*b);
+    }
+    public static void Division() {
+        int a = 856, b = 8;
+        System.out.println(a/b);
+    }
+    public static void Modulodivision() {
+        int a = 40, b = 3;
+        System.out.println(a%b);
+    }
+
+}
