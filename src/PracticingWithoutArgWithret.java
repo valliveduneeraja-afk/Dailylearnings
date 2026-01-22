ae2a980941e8278ba9a64face65ed835d089184a
Valli <valliveduneeraja@gmail.com>
2026-01-22 12:26:20 +0530

    PracticingWithoutArgWithret

---
+++ src/PracticingWithoutArgWithret.java
@@ -0,0 +1,12 @@
+class PracticingWithoutArg {
+    String msg(){
+        return "Neeraja";
+    }
+}
+class Pacticingret {
+    public static void main(String[] args) {
+        PracticingWithoutArg p1 = new PracticingWithoutArg();
+        String message = p1.msg();
+        System.out.println(message);
+    }
+}
