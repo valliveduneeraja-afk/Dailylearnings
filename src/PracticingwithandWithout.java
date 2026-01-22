4e40512d0798ac958e0d32c4c6cec181ffe0e724
Valli <valliveduneeraja@gmail.com>
2026-01-22 12:11:49 +0530

    WithArguementandWithoutReturn

---
+++ src/PracticingwithandWithout.java
@@ -0,0 +1,11 @@
+class Practicingwitharguement {
+    void mrg(String name) {
+        System.out.println(name);
+    }
+}
+class withoutreturnvalue {
+    public static void main(String[] args){
+        Practicingwitharguement p = new Practicingwitharguement();
+        p.mrg("Hi everyone");
+    }
+}
