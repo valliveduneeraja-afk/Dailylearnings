384587e08c344b0335451318fea829fd1849653a
Valli <valliveduneeraja@gmail.com>
2026-01-22 12:39:23 +0530

    PracticingWithoutArg

---
+++ src/PracticingWithoutArgWithoutRet.java
@@ -0,0 +1,11 @@
+class PracticingWithoutArgument {
+    void msg(){
+        System.out.println("Practicing arguments and return value");
+    }
+}
+class PracWithoutReturnValur {
+    public static void main(String[] args){
+        PracticingWithoutArgument obj = new PracticingWithoutArgument();
+        obj.msg();
+    }
+}
