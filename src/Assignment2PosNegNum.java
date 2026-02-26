1a704689c1d6f3b36b9d02478d1a203df0e43758
Valli <valliveduneeraja@gmail.com>
2026-02-26 12:32:47 +0530

    Assignment2PosNegNum

---
+++ src/Assignment2PosNegNum.java
@@ -0,0 +1,18 @@
+class Assignment2 {
+    public static void main(String[] args){
+        int num = 25;
+        int num1 = -8;
+        if (num > 0) {
+            System.out.println("25 is positive number");
+        }
+        else {
+            System.out.println("Num is a Positive number");
+        }
+        if (num1 < 0) {
+            System.out.println("-8 is a Negative");
+        }
+        else {
+            System.out.println("Num is a negative number");
+        }
+    }
+}
